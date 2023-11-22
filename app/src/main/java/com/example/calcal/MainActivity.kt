cfa7212a868c2a1c2f83713ce8bf93cf1f29e5ae
DESKTOP-JJHHJ8M\admin <pys0938@naver.com>
2023-11-22 18:01:55 +0900

    절대 색 안바뀜 내일은 포기함

--- app/src/main/java/com/example/calcal/MainActivity.kt
+++ app/src/main/java/com/example/calcal/MainActivity.kt
@@ -1,34 +1,36 @@
 package com.example.calcal
 
-import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.example.calcal.databinding.ActivityMainBinding
-import com.example.calcal.mainFrag.CalendarFragment
-import com.example.calcal.mainFrag.GraphFragment
-import com.example.calcal.mainFrag.MainFragment
-import com.example.calcal.mainFrag.MypageFragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        Log.d("MainActivity", "onCreate 호출됨")
+        binding.bottomNavigation.post {
+            navController = findNavController(R.id.my_nav)
+            binding.bottomNavigation.setupWithNavController(navController)
+        }
 
-        val navController = findNavController(R.id.my_nav)
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
 
-        NavigationUI.setupWithNavController(bottomNavigationView, navController)
-        bottomNavigationView.setupWithNavController(navController)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+//        navController = findNavController(R.id.my_nav)
+//        binding.bottomNavigation.setupWithNavController(navController)
 
         val callback = object : OnBackPressedCallback(true /* enabled by default */) {
             override fun handleOnBackPressed() {
@@ -40,11 +42,4 @@ class MainActivity : AppCompatActivity() {
         }
         onBackPressedDispatcher.addCallback(this, callback)
     }
-//    override fun onBackPressed() {
-//        if (!findNavController(R.id.my_nav).navigateUp()) {
-//            super.onBackPressed()
-//        }
-//    }
-
-
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,3 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 
 }
-
-
