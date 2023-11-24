430159c0478d545ebd8ce055da869f7d54e03f70
DESKTOP-JJHHJ8M\admin <pys0938@naver.com>
2023-11-24 18:37:43 +0900

    no message

--- app/src/main/java/com/example/calcal/mainFrag/CalendarFragment.kt
+++ app/src/main/java/com/example/calcal/mainFrag/CalendarFragment.kt
@@ -1,15 +1,26 @@
 package com.example.calcal.mainFrag
 
+import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
+import android.text.style.ForegroundColorSpan
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import com.example.calcal.R
+import com.example.calcal.databinding.FragmentCalendarBinding
+import com.google.android.material.datepicker.DayViewDecorator
+import com.prolificinteractive.materialcalendarview.CalendarDay
+import com.prolificinteractive.materialcalendarview.DayViewFacade
+import com.prolificinteractive.materialcalendarview.format.ArrayWeekDayFormatter
+import java.time.DayOfWeek
 
 class CalendarFragment : Fragment() {
-
-
+    private lateinit var binding: FragmentCalendarBinding
+    private var selectedStartSchedule: String = ""
+    private var selectedEndSchedule: String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -20,9 +31,112 @@ class CalendarFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentCalendarBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val dayDecorator = DayDecorator(requireContext())
+        val todayDecorator = TodayDecorator(requireContext())
+        val sundayDecorator = SundayDecorator()
+        val saturdayDecorator = SaturdayDecorator()
+        var selectedMonthDecorator = SelectedMonthDecorator(CalendarDay.today().month)
 
-        return inflater.inflate(R.layout.fragment_calendar, container, false)
+        binding.calendarView.addDecorators(dayDecorator, todayDecorator, sundayDecorator, saturdayDecorator, selectedMonthDecorator)
+
+        binding.calendarView.setWeekDayFormatter(ArrayWeekDayFormatter(resources.getTextArray(R.array.custom_weekdays)))
+
+        binding.calendarView.setHeaderTextAppearance(R.style.CalendarWidgetHeader)
+
+        binding.calendarView.setOnRangeSelectedListener { widget, dates ->
+            selectedStartSchedule = dates[0].date.toString()
+            selectedEndSchedule = dates[dates.size - 1].date.toString()
+        }
+        binding.calendarView.setTitleFormatter { day ->
+            val inputText = day.date
+            val calendarHeaderElements = inputText.toString().split("-")
+            val calendarHeaderBuilder = StringBuilder()
+
+            calendarHeaderBuilder.append(calendarHeaderElements[0]).append("년 ")
+                .append(calendarHeaderElements[1]).append("월")
+
+            calendarHeaderBuilder.toString()
+        }
+
+        binding.calendarView.setOnMonthChangedListener { widget, date ->
+            binding.calendarView.removeDecorators()
+            binding.calendarView.invalidateDecorators()
+
+            selectedMonthDecorator = SelectedMonthDecorator(date.month)
+            binding.calendarView.addDecorators(dayDecorator, todayDecorator, sundayDecorator, saturdayDecorator, selectedMonthDecorator)
+        }
+        binding.calendarView.setOnDateChangedListener { widget, date, selected ->
+            selectedStartSchedule = date.date.toString()
+            selectedEndSchedule = ""
+
+            if (!selected) {
+                selectedStartSchedule = ""
+            }
+        }
+
+    }
+    /* 선택된 날짜의 background를 설정하는 클래스 */
+    private inner class DayDecorator(context: Context) : DayViewDecorator {
+        private val drawable = ContextCompat.getDrawable(context,R.drawable.calendar_selector)
+        // true를 리턴 시 모든 요일에 내가 설정한 드로어블이 적용된다
+        override fun shouldDecorate(day: CalendarDay): Boolean {
+            return true
+        }
+
+        // 일자 선택 시 내가 정의한 드로어블이 적용되도록 한다
+        override fun decorate(view: DayViewFacade) {
+            view.setSelectionDrawable(drawable!!)
+        }
     }
 
+    /* 오늘 날짜의 background를 설정하는 클래스 */
+    private class TodayDecorator(context: Context): DayViewDecorator {
+        private val drawable = ContextCompat.getDrawable(context,R.drawable.calendar_circle_gray)
+        private var date = CalendarDay.today()
+        override fun shouldDecorate(day: CalendarDay?): Boolean {
+            return day?.equals(date)!!
+        }
+        override fun decorate(view: DayViewFacade?) {
+            view?.setBackgroundDrawable(drawable!!)
+        }
+    }
 
+    /* 이번달에 속하지 않지만 캘린더에 보여지는 이전달/다음달의 일부 날짜를 설정하는 클래스 */
+    private inner class SelectedMonthDecorator(val selectedMonth : Int) : DayViewDecorator {
+        override fun shouldDecorate(day: CalendarDay): Boolean {
+            return day.month != selectedMonth
+        }
+        override fun decorate(view: DayViewFacade) {
+            view.addSpan(ForegroundColorSpan(ContextCompat.getColor(requireContext(), R.color.text40)))
+        }
+    }
+
+    /* 일요일 날짜의 색상을 설정하는 클래스 */
+    private class SundayDecorator : DayViewDecorator {
+        override fun shouldDecorate(day: CalendarDay): Boolean {
+            val sunday = day.date.with(DayOfWeek.SUNDAY).dayOfMonth
+            return sunday == day.day
+        }
+
+        override fun decorate(view: DayViewFacade) {
+            view.addSpan(object: ForegroundColorSpan(Color.RED){})
+        }
+    }
+
+    /* 토요일 날짜의 색상을 설정하는 클래스 */
+    private class SaturdayDecorator : DayViewDecorator {
+        override fun shouldDecorate(day: CalendarDay): Boolean {
+            val saturday = day.date.with(DayOfWeek.SATURDAY).dayOfMonth
+            return saturday == day.day
+        }
+
+        override fun decorate(view: DayViewFacade) {
+            view.addSpan(object:ForegroundColorSpan(Color.BLUE){})
+        }
+    }
 }
