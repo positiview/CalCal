83c0ec05b43b071b8e7a0f76a81ff249290c5e10
JOO0507 <wlduswn12@gmail.com>
2023-12-01 09:28:02 +0900

    g

--- app/src/main/java/com/example/calcal/subFrag/SearchAddressDialog.kt
+++ app/src/main/java/com/example/calcal/subFrag/SearchAddressDialog.kt
@@ -194,25 +194,25 @@ class SearchAddressDialog(private val myArea: String) :DialogFragment() {
             }
         }
 
-       binding.directChooseOnMap.setOnClickListener {
-           val fragment = DirectSearchMapFragment()
-
-           // 현재 위치 정보를 전달
-           fragment.setCurrentLocation(locations)
-
-           // 선택된 주소값을 DirectSearchMapFragment로 전달
-           fragment.setSelectedAddress(clickedTextView?.text.toString())
-
-           // selectedItemDTO를 Bundle에 담아서 DirectSearchMapFragment로 전달
-           selectedItemDTO?.let {
-               val bundle = Bundle()
-               bundle.putParcelable("itemDTO", it)
-               fragment.arguments = bundle
-           }
-
-           fragment.show(parentFragmentManager, "DirectSearchMapFragment")
-           dismiss()
-       }
+//       binding.directChooseOnMap.setOnClickListener {
+//           val fragment = DirectSearchMapFragment()
+//
+//           // 현재 위치 정보를 전달
+//           fragment.setCurrentLocation(locations)
+//
+//           // 선택된 주소값을 DirectSearchMapFragment로 전달
+//           fragment.setSelectedAddress(clickedTextView?.text.toString())
+//
+//           // selectedItemDTO를 Bundle에 담아서 DirectSearchMapFragment로 전달
+//           selectedItemDTO?.let {
+//               val bundle = Bundle()
+//               bundle.putParcelable("itemDTO", it)
+//               fragment.arguments = bundle
+//           }
+//
+//           fragment.show(parentFragmentManager, "DirectSearchMapFragment")
+//           dismiss()
+//       }
 
 
 
